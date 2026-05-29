bcdc3b1bb7fc71aae3faea194b855fcb062bdc22
ianiket005 <ianiket.patel007@gmail.com>
2026-05-29 10:34:26 +0530

    p1

---
+++ Day 1/p1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int n,i,sum=0;
+    printf("Enter the number of elements: ");
+    scanf("%d",&n); 
+    for(i=1;i<=n;i++){
+        sum+=i;
+    }
+    printf("Sum of first %d natural numbers is: %d",n,sum);
+    return 0;
+
+}
